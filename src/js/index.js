fee90659d752472085c59a74a86ea3f75e97809e
Marina Rak <rakmarina1995@gmail.com>
2022-12-10 16:18:48 +0300

    fix: adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,51 @@
-let rule = [],
-    srcText = document.getElementById('src-text'),
-    transText = document.getElementById('trans-text'),
-    translateBtn = document.getElementById('translate-btn'),
-    clearBtn = document.getElementById('clear-btn'),
-    copyBtn = document.getElementById('copy-btn');
+'use strict'
+window.addEventListener("load", function (event) {
+    let rule = [],
+        srcText = document.getElementById('src-text'),
+        transText = document.getElementById('trans-text'),
+        translateBtn = document.getElementById('translate-btn'),
+        clearBtn = document.getElementById('clear-btn'),
+        copyBtn = document.getElementById('copy-btn');
+    init();
 
-Promise.all(
-    [
-        fetch('rule.json')
-    ]
-)
-    .then(async ([ruleResponse]) => {
-        const ruleJson = await ruleResponse.json();
-        return [ruleJson];
-    })
-    .then(response => {
-        rule = response[0];
-        init();
-    });
-
-
-function init() {
-    // srcText.onkeydown = (e) => {
-    //     const reg = /[A-Za-z]/;
-    //     console.log(e.key);
-    //     if (reg.test(e.key)) {
-    //         console.log('Включите кириллицу')
-    //     }
-    // }
-    translateBtn.onclick = (e) => {
-        let src = Array.from(srcText.value);
-        let arr = src.map(letter => {
-            let change = rule.find(item => {
-                return item.letter == letter;
-            })
-            if (change && change.hasOwnProperty('value')) {
-                return letter = change.value;
-
-
-            } else {
-                return letter;
+    async function getData() {
+        const response = await fetch('rule.json');
+        if (response && response.status === 200) {
+            const result = await response.json();
+            if (result && !result.error) {
+                rule = result;
+                return;
             }
-        })
-        transText.value = arr.join('');
+        } else {
+            console.log('Не удалось получить данные!')
+        }
     }
-    clearBtn.onclick = (e) => {
-        srcText.value = "";
-        transText.value = "";
-    }
-    copyBtn.onclick = (e) => {
-        transText.select();
-        document.execCommand("copy");
 
+    function init() {
+        getData();
+        translateBtn.onclick = (e) => {
+            let arr = Array.from(srcText.value).map(letter => {
+                let change = rule.find(item => {
+                    return item.letter === letter;
+                })
+                if (change && change.hasOwnProperty('value')) {
+                    return letter = change.value;
+
+                } else {
+                    return letter;
+                }
+            })
+            transText.value = arr.join('');
+        };
+        clearBtn.onclick = (e) => {
+            srcText.value = "";
+            transText.value = "";
+        }
+        copyBtn.onclick = (e) => {
+            transText.select();
+            document.execCommand("copy");
+
+        }
     }
-}
+
+})
